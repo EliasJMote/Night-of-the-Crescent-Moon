a809a4c4d1d9ac96d5f0774ac99c30776240a3ec
Elias James Mote <rulerofchaosstudios@gmail.com>
2019-06-13 17:35:55 -0700

    V0.2.0 - Added Forest of Spirits area, will o' the wisp npc Cassandra, dialogue and key term systems

--- main.lua
+++ main.lua
@@ -3,6 +3,15 @@ function checkWalkableDescription()
     for k,v in ipairs(locations[curLocation]["walkDesc"]) do
         if(coords.x == v.x and coords.y == v.y) then
             writeToTextDisplay(v.des)
+            if(v.img ~= nil) then
+                npcImg = v.img
+                isTalking = true
+            end
+        end
+        if(v.img == nil) then
+            isTalking = false
+            isSayingWord = false
+            curTermToLearn = nil
         end
     end
 end
@@ -16,13 +25,24 @@ function checkSearchableDescription()
     end
 end
 
+function addKeyTerm(term)
+    if(term == nil) then
+        return false
+    end
+    
+    -- Check if the key term is in the list already
+    for k,v in ipairs(keyTerms) do
+        if(v["text"] == term["text"]) then
+            return false
+        end
+    end
+    table.insert(keyTerms,term)
+    return true
+end
+
 -- Write to the game's text display
 function writeToTextDisplay(text)
-    
-    -- Reverse the text order
-    --text = revArray(text)
-    
-    --print(#text)
+
     -- For each line in the text, insert them into the text display at the start in reverse order
     table.insert(curText, "\n")
     for k,v in ipairs(text) do
@@ -64,37 +84,78 @@ function love.load()
     -- Locations: Ancient Tower, Bedroom, Crystal Cave, Emerald Sea, Forest of Spirits, Forgotten Pit, Lost Hills
     curLocation = "Bedroom"
     coords = {x=0,y=0}
-    keyTerms = {"Eye of Truth", "Lantern"}
+    --keyTerms = {"Eye of Truth", "Lantern"}
+    keyTerms = {}
+    
+    keyTermMap = {
+                    f="forest",
+                 }
+    
+    curTermToLearn = nil
     
     -- Game locations and their information
     locations = {
                     Ancient_Tower =     {
-                                            width = 1,
-                                            height = 3,
                                             rooms = {{x=0,y=0},{x=0,y=1},{x=0,y=2},},
-                                            --mapImage = love.graphics.newImage("Bedroom_Map.png"),
-                                            searDesc = {},
-                                            walkDesc = {{x=0,y=0,des=""},}
+                                            mapImage = love.graphics.newImage("Ancient_Tower_Map.png"),
+                                            roomImage = love.graphics.newImage("Ancient_Tower.png"),
+                                            searDesc = {{x=0,y=0,des={"You take the long road from the Ancient Tower back to the central Hub.","Several paths sprawl before you in the dim moonlight.","Where do you wish to go?","[A]ncient Tower", "[C]rystal Cave", "[E]merald Sea", "[F]orest of Spirits", "[L]ost Hills", "F[o]rgotten Pit"}}},
+                                            walkDesc = {{x=0,y=0,des={"A crumbling tower stands before you."}}}
                                         },
                     Bedroom =   {
-                                    width = 2,
-                                    height = 2,
                                     rooms = {{x=0,y=0},{x=1,y=0},{x=0,y=1},{x=1,y=1}},
                                     mapImage = love.graphics.newImage("Bedroom_Map.png"),
                                     roomImage = love.graphics.newImage("Title Picture.png"),
-                                    searDesc = {{x=1,y=1,conds={},des={"You step out of the open window. Several paths sprawl before you in the dim moonlight.","Where do you wish to go?","[A]ncient Tower", "[C]rystal Cave", "[E]merald Sea", "[F]orest of Fallen Spirits", "[L]ost Hills", "Forgotten [P]it"}}},
+                                    searDesc = {{x=1,y=1,conds={},des={"You step out of the open window. Several paths sprawl before you in the dim moonlight.","Where do you wish to go?","[A]ncient Tower", "[C]rystal Cave", "[E]merald Sea", "[F]orest of Spirits", "[L]ost Hills", "F[o]rgotten Pit"}}},
                                     walkDesc = {{x=0,y=0,des={"There is a bed in front of you"}},{x=1,y=1,des={"An open window sits in front of you. The curtain blows in the cold wind."}}}
                                 },
                     Crystal_Cave = {},
                     Emerald_Sea = {},
-                    Forest_Spirits = {},
+                    Forest_of_Spirits = {
+                                            rooms = {{x=0,y=0},{x=1,y=0},{x=2,y=0},{x=0,y=1},{x=1,y=1},{x=2,y=1},{x=0,y=2},{x=1,y=2},{x=2,y=2},},
+                                            mapImage = love.graphics.newImage("Forest_of_Spirits_Map.png"),
+                                            roomImage = love.graphics.newImage("Forest_of_Spirits.png"),
+                                            searDesc =  {},
+                                            walkDesc =  {
+                                                            {
+                                                                x=0,y=0,des={
+                                                                                "You enter into a very relaxing and soothing forest overflowing with a thin mist. The spirits of the",
+                                                                                "forest fly gracefully through the sky as small balls of light, conjuring a dazzling spectacle. The air",
+                                                                                "is cool and crisp to the senses; your nose is filled with a fresh, earth scent. You feel at peace",
+                                                                                "here, as though the sleeping remains of the fallen buried deep within the earth are protecting",
+                                                                                "this area, creating a safe haven."
+                                                                            }
+                                                            },
+                                                            {
+                                                                x=2,y=2,des={
+                                                                                "You approach one of the spirits of the forest."
+                                                                            },
+                                                                img = {love.graphics.newImage("Will_o_the_Wisp_1.png"),
+                                                                    love.graphics.newImage("Will_o_the_Wisp_2.png"),
+                                                                    love.graphics.newImage("Will_o_the_Wisp_3.png"),
+                                                                    love.graphics.newImage("Will_o_the_Wisp_4.png"),
+                                                                    love.graphics.newImage("Will_o_the_Wisp_5.png"),},
+                                                            },
+                                                        },
+                                            npc =   {
+                                                        x=2,
+                                                        y=2,
+                                                        dialogue=   {
+                                                                        default={
+                                                                                    text={"I am Cassandra, one of the spirits of the \"Forest\""},
+                                                                                    term={text="forest",command="[F]orest"}
+                                                                                },
+                                                                        forest= {
+                                                                                    text={"This forest is a haven for those of us who wander the earth untethered by mortality."},
+                                                                                }
+                                                                    }
+                                                    }
+                                        },
                     Forgotten_Pit = {},
                     Graveyard = {},
                     Hub =   {
-                                width = 1,
-                                height = 1,
                                 rooms = {{x=0,y=0}},
-                                --mapImage = {},
+                                mapImage = love.graphics.newImage("Hub_Map.png"),
                                 searDesc = {},
                                 walkDesc = {{x=0,y=0,des=""}},
                                 roomImage = love.graphics.newImage("Hub.png"),
@@ -133,9 +194,17 @@ function love.draw()
         love.graphics.rectangle("line", 20, 20, 400, 150, 10)
         
         if(state == "title") then
-            love.graphics.draw(titleScreen, 50, 33)
+            love.graphics.draw(titleScreen, 50-20, 33-3)
         elseif(state == "game") then
-            love.graphics.draw(locations[curLocation]["roomImage"], 50, 33)
+            if(isTalking == false) then
+                love.graphics.draw(locations[curLocation]["roomImage"], 50-20, 33-3)
+            else
+                if(type(npcImg) == "table") then
+                    love.graphics.draw(npcImg[math.floor(timer/20) % #npcImg + 1], 50-20, 33-3)
+                else
+                    love.graphics.draw(npcImg, 50-20, 33-3)
+                end
+            end
         end
         
         -- draw auxilliary screen
@@ -144,24 +213,24 @@ function love.draw()
         -- draw coordinates
         love.graphics.print("Coords        " .. coords.x .. "," .. coords.y, 450, 150)
         
-        if(isTalking == false) then
+        --if(isTalking == false) then
         
-            -- draw map text
-            love.graphics.print("Map", 520, 30)
-            
-            -- draw map
-            if(curLocation == "Bedroom") then
-                love.graphics.draw(locations[curLocation]["mapImage"], 450, 115)
-            end
-            
-            -- draw coordinates indicator
-            if(timer % 60 < 60 / 2) then
-                love.graphics.rectangle("fill", 450 + 16 * coords.x, 131 - 16 * coords.y, 16, 16)
-            end
-        else
-            -- draw key terms text
-            love.graphics.print("Key Terms", 500, 30)
+        -- draw map text
+        love.graphics.print("Map", 520, 30)
+        
+        -- draw map
+        --if(curLocation == "Bedroom") then
+        love.graphics.draw(locations[curLocation]["mapImage"], 450, 115 - 16)
+        --end
+        
+        -- draw coordinates indicator
+        if(timer % 60 < 60 / 2) then
+            love.graphics.rectangle("fill", 450 + 16 * coords.x, 131 - 16 * coords.y, 16, 16)
         end
+        --else
+            -- draw key terms text
+            --love.graphics.print("Key Terms", 500, 30)
+        --end
         
         -- draw text screen
         love.graphics.rectangle("line", 20, 190, 600 , 150, 10)
@@ -231,11 +300,58 @@ function love.keypressed(key, scancode, isrepeat)
             -- Search area
         if(key == "return") then
             checkSearchableDescription()
-            if(curLocation == "Bedroom" and coords.x == 1 and coords.y == 1) then
+            if((curLocation == "Bedroom" and coords.x == 1 and coords.y == 1)
+                or (curLocation == "Ancient_Tower" and coords.x == 0 and coords.y == 0)) then
                 curLocation = "Hub"
-                --writeToTextDisplay({"Several paths sprawl before you in the dim moonlight.","Where do you wish to go?","[A]ncient Tower", "[C]rystal Cave", "[E]merald Sea", "[F]orest of Fallen Spirits", "[L]ost Hills", "Forgotten [P]it"})
                 coords.x = 0
                 coords.y = 0
+            end
+            if(isTalking) then
+                writeToTextDisplay(locations[curLocation]["npc"]["dialogue"]["default"]["text"])
+                curTermToLearn = locations[curLocation]["npc"]["dialogue"]["default"]["term"]
+                writeToTextDisplay({"[S]ay","[L]earn"})
+            end
+        end
+        
+        if(isSayingWord and key ~= "p") then
+            --if(string.match(key,'[a-z]')) then
+            local term = keyTermMap[key]
+            for k,v in ipairs(keyTerms) do
+                if(v["text"] == term) then
+                    if(locations[curLocation]["npc"]["dialogue"][term] ~= nil) then
+                        writeToTextDisplay(locations[curLocation]["npc"]["dialogue"][term]["text"])
+                    end
+                end
+            end
+            --print(term)
+        end
+        
+        if(key == "s") then
+            if(isTalking) then
+                isSayingWord = true
+                curTermToLearn = nil
+                writeToTextDisplay({"Select a word to say:"})
+                for k,v in ipairs(keyTerms) do
+                    writeToTextDisplay({v["command"]})
+                end
+            end
+        end
+        
+        if(key == "l") then
+            if(isTalking) then
+                if(addKeyTerm(curTermToLearn)) then
+                    writeToTextDisplay({"Learned key term: " .. curTermToLearn["text"]})
+                end
+            end
+        end
+        
+        if(curLocation == "Hub") then
+            if(key == "a") then
+                curLocation = "Ancient_Tower"
+                checkWalkableDescription()
+            elseif(key == "f") then
+                curLocation = "Forest_of_Spirits"
+                checkWalkableDescription()
             end
         end
         
